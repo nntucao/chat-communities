868b5ec01c18d3086dedc2c6837c296f556eaca1
nntucao <ngtu.caong@gmail.com>
2021-04-02 20:46:30 +0200

    Set up login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
-import ChatFeed from './components/ChatFeed';
+import ChatFeed  from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
   return (
     <ChatEngine
       height="100vh"
       projectID="65abc1dc-166f-423f-b67c-0b1cf1e355b3"
-      userName="nntc"
-      userSecret="123123"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
     />
   );
